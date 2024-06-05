b4a831bfe87fcf742d724d6eb7357cd5074de99f
Mohsporty <m.alassdi@gmail.com>
2024-06-05 18:41:30 +0300

    this all about first update

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    
+    
 
+    // this for top labael i add in story board
+    @IBOutlet weak var turnLabel: UILabel!
+    // right now i add the euch label for 9 square   in the schedule
+    // this is first line is start with (  a )
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    
+    // start secned line with (b)
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    // this is thered line is start with (c)
+
+    @IBOutlet weak var c1: UIButton!
+
+    @IBOutlet weak var c2: UIButton!
+    
+  
+    @IBOutlet weak var c3: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    // for add action for euch square
 
-
+    
+    @IBAction func boardTapAction(_ sender: UIButton) {
+    }
+    
+    
 }
 
