bfca38650eb07463c7bd7ca065c1282372172cad
Mohsporty <m.alassdi@gmail.com>
2024-06-05 21:27:13 +0300

    add all with one bug is font is small i will investigate on it

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -10,8 +10,12 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    // 1 * this is start coding and build project one by one
     
-    
+    enum Turn {
+        case Nought
+        case Cross
+    }
     
     
     
@@ -31,25 +35,48 @@ class ViewController: UIViewController {
     // this is thered line is start with (c)
 
     @IBOutlet weak var c1: UIButton!
-
     @IBOutlet weak var c2: UIButton!
-    
-  
     @IBOutlet weak var c3: UIButton!
     
-    
+    // starting add varibale and functions
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    // for add action for euch square
+    // 2 * for add action for euch square
+    
+    var firstTurn = Turn.Cross
+    var currentTurn = Turn.Cross
+    
+    // 3 * this add informations of o and x for tap and show in app
+    
+    var NOUGHT = "O"
+    var CROSS = "X"
 
     
     @IBAction func boardTapAction(_ sender: UIButton) {
+        
+        addToBoard(sender)
+        
     }
     
+    // this function to build about UI BUtton i build and tap to add the your varebale on it
+    func addToBoard(_ sender: UIButton)  {
+        if(sender.title(for: .normal) == nil){
+            if(currentTurn == Turn.Nought){
+                sender.setTitle(NOUGHT, for: .normal)
+                currentTurn = Turn.Cross
+                turnLabel.text = CROSS
+            }
+            if(currentTurn == Turn.Cross){
+                sender.setTitle(CROSS, for: .normal)
+                currentTurn = Turn.Nought
+                turnLabel.text = NOUGHT
+            }
+        }
+    }
     
 }
 
