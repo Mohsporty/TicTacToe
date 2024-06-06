293da7412165a032da55b45b5631c1dcf54cb392
Mohsporty <m.alassdi@gmail.com>
2024-06-06 04:44:08 +0300

    add the answer in muliteple func

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -32,13 +32,24 @@ class ViewController: UIViewController {
     
     var NOUGHT = "O"
     var CROSS = "X"
-    
+    var board = [UIButton]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        initBoard()
+    }
+    func initBoard()  {
+        board.append(a1)
+        board.append(a2)
+        board.append(a3)
+        board.append(b1)
+        board.append(b2)
+        board.append(b3)
+        board.append(c1)
+        board.append(c2)
+        board.append(c3)
     }
     
     // Do any additional setup after loading the view.
@@ -46,7 +57,115 @@ class ViewController: UIViewController {
     @IBAction func boardTapAction(_ sender: UIButton) {
         
         addToBoard(sender)
+        
+        
+        
+        if(fullBoard())
+        {
+            resultAlert(title: "Draw")
+        }
     }
+    
+    
+    func checkForVictory(_ s : String) -> Bool {
+        
+        
+        // horizontal victory answer
+        
+        
+        if thisSymbol(a1, s) && thisSymbol(a2, s) && thisSymbol(a3, s){
+            return true
+        }
+        if thisSymbol(b1, s) && thisSymbol(b2, s) && thisSymbol(b3, s){
+            return true
+        }
+        if thisSymbol(c1, s) && thisSymbol(c2, s) && thisSymbol(c3, s){
+            return true
+        }
+        
+        // verticaly  victory answer
+        
+        
+        if thisSymbol(a1, s) && thisSymbol(b1, s) && thisSymbol(c1, s){
+            return true
+        }
+        if thisSymbol(a2, s) && thisSymbol(b2, s) && thisSymbol(c2, s){
+            return true
+        }
+        if thisSymbol(a3, s) && thisSymbol(b3, s) && thisSymbol(c3, s){
+            return true
+        }
+        
+    
+        // Dignoal  victory answer
+        
+        
+        if thisSymbol(a1, s) && thisSymbol(b2, s) && thisSymbol(c3, s){
+            return true
+        }
+        if thisSymbol(a3, s) && thisSymbol(b2, s) && thisSymbol(c1, s){
+            return true
+        }
+    
+    
+        
+        return false
+    }
+    
+    func thisSymbol(_ button: UIButton, _ symbol: String ) -> Bool {
+        return button.title(for: .normal) == symbol
+    }
+    
+    
+    
+    func resultAlert(title : String)  {
+        let ac = UIAlertController(title: title, message: nil, preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "Reset", style: .default, handler: {(_) in
+            self.restBoard()
+        }))
+        
+        self.present(ac, animated: true )
+        
+    }
+    
+    func restBoard() {
+        for button in board {
+            button.setTitle(nil, for: .normal)
+            button.isEnabled = true
+        }
+        if (firstTurn == Turn.Nought){
+            firstTurn = Turn.Cross
+            turnLabel.text = CROSS
+            
+        }
+      else   if (firstTurn == Turn.Cross){
+            firstTurn = Turn.Nought
+            turnLabel.text = NOUGHT
+            
+        }
+        
+        
+        
+        currentTurn = firstTurn
+    }
+    
+    
+    
+    
+    
+    func fullBoard() -> Bool {
+        for button in board
+        {
+            if button.title(for: .normal) == nil
+            {
+                return false
+            }
+        }
+        return true
+    }
+    
+    
+    
     
     func addToBoard(_ sender: UIButton) {
         if(sender.title(for: .normal) == nil)
@@ -65,7 +184,7 @@ class ViewController: UIViewController {
                 turnLabel.text = NOUGHT
                 
             }
-        
+            sender.isEnabled = false
         }
     }
 }
