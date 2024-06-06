11d9f1190222afe03fcadf89a378354aab23fa01
Mohsporty <m.alassdi@gmail.com>
2024-06-06 04:02:39 +0300

    build new with simple code also play without score and more

--- TicTacToe/ViewController.swift
+++ TicTacToe/ViewController.swift
@@ -9,20 +9,64 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var turnLabel: UILabel!
     
-
+    enum Turn {
+        case Nought
+        case Cross
+    }
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var a1: UIButton!
+    @IBOutlet weak var a2: UIButton!
+    @IBOutlet weak var a3: UIButton!
+    @IBOutlet weak var b1: UIButton!
+    @IBOutlet weak var b2: UIButton!
+    @IBOutlet weak var b3: UIButton!
+    @IBOutlet weak var c1: UIButton!
+    @IBOutlet weak var c2: UIButton!
+    @IBOutlet weak var c3: UIButton!
+    
+    
+    var firstTurn = Turn.Cross
+    var currentTurn = Turn.Cross
+    
+    var NOUGHT = "O"
+    var CROSS = "X"
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-      
-    
-        }
         
-        // Do any additional setup after loading the view.
+        
+    }
     
+    // Do any additional setup after loading the view.
     
+    @IBAction func boardTapAction(_ sender: UIButton) {
+        
+        addToBoard(sender)
+    }
     
-    
+    func addToBoard(_ sender: UIButton) {
+        if(sender.title(for: .normal) == nil)
+        {
+            if(currentTurn == Turn.Nought)
+            {
+                sender.setTitle(NOUGHT, for: .normal)
+                currentTurn = Turn.Cross
+                turnLabel.text = CROSS
+                
+            }
+        else if(currentTurn == Turn.Cross)
+            {
+                sender.setTitle(CROSS, for: .normal)
+                currentTurn = Turn.Nought
+                turnLabel.text = NOUGHT
+                
+            }
+        
+        }
+    }
 }
 
